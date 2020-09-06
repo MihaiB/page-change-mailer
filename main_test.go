bd5c2ca355748db38391c08cd0a2172941bc35bf
Mihai Borobocea <138839+MihaiB@users.noreply.github.com>
2020-09-07 00:00:35 +0300

    add tests for function shouldEmail() (#11)

---
+++ main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+)
+
+func TestShouldEmailNoHistory(t *testing.T) {
+	d := t.TempDir()
+	filename := filepath.Join(d, "no-such-file")
+	for _, s := range []string{"", "\n", "hello"} {
+		val, err := shouldEmail(filename, []byte(s))
+		if err != nil {
+			t.Error(err)
+		}
+		if val {
+			t.Error("wants to email when there is no " +
+				"previous version of the page")
+		}
+	}
+}
+
+func TestShouldEmailError(t *testing.T) {
+	d := t.TempDir()
+	if _, err := shouldEmail(d, nil); err == nil {
+		t.Error("Expected an error when the filename is a directory")
+	}
+}
+
+func TestShouldEmailCompareContent(t *testing.T) {
+	d := t.TempDir()
+	f := filepath.Join(d, "file")
+	oldContent, newContent := []byte("old"), []byte("new")
+	if err := ioutil.WriteFile(f, oldContent, 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	if val, err := shouldEmail(f, oldContent); val || err != nil {
+		t.Error(val, err)
+	}
+	if val, err := shouldEmail(f, newContent); !val || err != nil {
+		t.Error(val, err)
+	}
+}
